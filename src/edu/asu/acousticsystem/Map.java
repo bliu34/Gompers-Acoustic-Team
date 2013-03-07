ec192e2ba0d98711ce7a6e9c383c1848d168079d
Dominic Smith <dsmith20@asu.edu>
2013-03-07 16:36:21 -0700

    Revert "Revert "Rebuilt the structure so we can hopefully import. May need to rollback if it breaks things.""
    This reverts commit de764843cf164b2f547f3386e4ea6683ecd86710.

---
+++ src/edu/asu/acousticsystem/Map.java
@@ -0,0 +1,19 @@
+package edu.asu.acousticsystem;
+
+public class Map {
+	
+	private double length;
+	private double width;
+	Map()
+	{
+		length = 1;
+		width = 1;
+	}
+	
+	Map(double length, double width)
+	{
+		this.length = length;
+		this.width = width;
+	}
+
+}
---
+++ src/edu/asu/acousticsystem/WorldMap.java
@@ -0,0 +1,30 @@
+package edu.asu.acousticsystem;
+
+/*
+Author: Jian Fang		
+Date: December 10, 2010
+Title of program: WorldMap.java
+Version: [unknown]
+Form: [Java code]
+Availability: http://code.google.com/p/jianwikis/wiki/AStarAlgorithmForPathPlanning
+Date Accessed: Jan. 21, 2013
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+//aAmatrix to hold all positions as follows.
+public class WorldMap {
+    int rowNum;
+    int columnNum;
+
+    String[][] map;
+    Map<String, MatrixPosition> index;
+
+    public WorldMap(int rowNum, int columnNum) {
+        this.rowNum = rowNum;
+        this.columnNum = columnNum;
+        map = new String[rowNum][columnNum];
+        index = new HashMap<String, MatrixPosition>();
+    }
+}
