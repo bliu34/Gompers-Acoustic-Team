e66b3b744d8825cafd739a77d9c991ec3b923f9f
Benson Liu <bliu@MacBook-Air.local>
2013-03-17 23:51:19 -0700

    Added Intent import.

--- src/edu/cst316/gompersacousticsystem/MainActivity.java
+++ src/edu/cst316/gompersacousticsystem/MainActivity.java
@@ -5,6 +5,7 @@ import edu.cst316.gompersacousticsystem.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
