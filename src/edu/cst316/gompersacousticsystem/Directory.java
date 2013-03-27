d2bbd28a8a4342c35bf9c46af4cddd78f975c31d
Matthew Alvarado <mlalvar3@asu.edu>
2013-03-27 14:42:57 -0700

    Commented out setLatitude and setLongitude to run

--- src/edu/cst316/gompersacousticsystem/Directory.java
+++ src/edu/cst316/gompersacousticsystem/Directory.java
@@ -13,28 +13,28 @@ public class Directory {
 	
 	double officeLat = 33.346534;
 	double officeLong = -112.116128;
-	officeNode.setLatitude(officeLat);
-	officeNode.setLongitude(officeLong);
+	//officeNode.setLatitude(officeLat);
+	//officeNode.setLongitude(officeLong);
 	
 	double restLat = 33.346534;
 	double restLong = -112.116128;
-	restroomNode.setLatitude(restLat);
-	restroomNode.setLongitude(restLong);
+	//restroomNode.setLatitude(restLat);
+	//restroomNode.setLongitude(restLong);
 	
 	double cafeteriaLat = 33.346534;
 	double cafeteriaLong = -112.116128;
-	cafeteriaNode.setLatitude(cafeteriaLat);
-	cafeteriaNode.setLongitude(cafeteriaLong);
+	//cafeteriaNode.setLatitude(cafeteriaLat);
+	//cafeteriaNode.setLongitude(cafeteriaLong);
 	
 	double homeLat = 33.346534;
 	double homeLong = -112.116128;
-	homeNode.setLatitude(homeLat);
-	homdNode.setLongitude(homeLong);
+	//homeNode.setLatitude(homeLat);
+	//homdNode.setLongitude(homeLong);
 	
 	double frontLat = 33.346534;
 	double frontLong = -112.116128;
-	frontNode.setLatitude(frontLat);
-	frontNode.setLongitude(frontLat);
+	//frontNode.setLatitude(frontLat);
+	//frontNode.setLongitude(frontLat);
 	
 	
 	double getCaseManagerLat(){
