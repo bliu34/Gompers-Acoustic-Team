7ce0ca36701ec2595dd42bc7bd532f6ae5a48845
Dominic Smith <dsmith20@asu.edu>
2013-04-02 19:54:05 -0700

    Simple floor plan
    Drew a simple floor plan that isn't all cluttered with words and such.
    Didn't put in the locations yet.

--- gen/edu/cst316/gompersacousticsystem/R.java
+++ gen/edu/cst316/gompersacousticsystem/R.java
@@ -23,7 +23,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     }
     public static final class drawable {
         public static final int floorplan_edited=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int floorplan_simple=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int GestureEvent=0x7f080001;
