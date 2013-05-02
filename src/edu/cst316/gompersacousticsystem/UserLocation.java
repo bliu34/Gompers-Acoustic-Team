b21ff210eb877a7ed1ccce5b52d617b7443504c6
bliu34 <bliu34@asu.edu>
2013-05-01 18:29:58 -0700

    Defensive programming, added initial input of true, and assert statement.

--- src/edu/cst316/gompersacousticsystem/UserLocation.java
+++ src/edu/cst316/gompersacousticsystem/UserLocation.java
@@ -1,24 +1,27 @@
 package edu.cst316.gompersacousticsystem;
 
-import android.R.bool;
 import edu.cst316.gompersacousticsystem.RouterTrilateration;
 import edu.cst316.gompersacousticsystem.Signal;
 
 public class UserLocation {
+	
 	Signal signalStrength = new Signal(GestureActivity.getContext());
 	Router router1 = new Router("myqwest3331", signalStrength);
 	Router router2 = new Router("dlink", signalStrength);
 	Router router3 = new Router("belkin-router", signalStrength);
 	RouterTrilateration trilateration = new RouterTrilateration(router1, router2, router3);
+	
 	boolean tracking;
 
 	public UserLocation() {
-		
+	
 	}
 	
 	public void setTrackingMessage(boolean tracking){
 		
 		this.tracking = tracking;
+		tracking = true;
+		
 		
 		if (tracking == true) {
 			
@@ -29,9 +32,11 @@ public class UserLocation {
 		else if(tracking == false){
 			System.out.println("Unable to track location.");
 		}
+		
+		assert tracking = true : "Tracking works";
 
 	}
-
+	
 }
 
 //CODE REVIEW: Matthew Alvarado
